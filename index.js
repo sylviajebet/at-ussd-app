fd6fc289469856355bed02380519f609b68c5dda
sylviajebet <sylviajebett@gmail.com>
2024-03-16 13:37:22 +0300

    Changes added in main file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
 const UssdMenu = require('ussd-menu-builder');
 let menu = new UssdMenu();
 const express = require('express')
@@ -9,119 +11,20 @@ const PORT = process.env.PORT;
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
- // Authentication
-const credentials = {
+ // AT Authentication
+ const credentials = {
     apiKey: process.env.API_KEY, // API Key here 
     username: process.env.USERNAME // Username: 'sandbox' for the test environment, App username for live
 };
 
-// Require the AT package
-const AfricasTalking = require("africastalking")(credentials);
-
-// Define inputs
-let regNumber = []
-let cname = []
-let lessons = []
-
-// Defining states
-menu.startState({
-    run: () => {
-        regNumber = []
-        cname = []
-        lessons =[]
-
-        // Welcome Screen      
-        menu.con(' Welcome to our USSD demo! We\'ll be doing the semester evaluation '+
-                   '\n Reply with 1 to continue:' +
-                 '\n1. Start evaluation'
-            );
-    },
-    // Next object links to next state based on user input
-    next: {
-        '1': 'regnumber',
-        
-    }
-});
-
-//Details collection
-menu.state('regnumber', {
-    run: () => {
-        menu.con('Please enter your registration number: ')
-    },
-    next: {
-        '*[a-zA-Z]+': 'regNumber'
-    }
-})
-
-menu.state('regNumber', {
-    run: () => {
-        regNumber.push(String(menu.val))
-        menu.con(`Welcome ${regNumber[0]}! Your number is here**. Enter your course name:`)
-    },
-    next: {
-        
-        '*[a-zA-Z]+': 'cname'
-    }
-})
-
-menu.state('cname', {
-    run: () => {
-        cname.push(String(menu.val))
-        menu.con(`Thanks, good to know you take ${cname[0]}.
-        \n Rate the quality of lessons on a scale of 1 - 5: `);
-    },
-    next: {
-        
-        '*\\d+': 'lessons'
-    }
-})
-
-menu.state('lessons', {
-    run: () => {
-        lessons.push(Number(menu.val))
-        //Check Lessons rating brackets
-        // Parse message for sending text on completion
-        if(lessons>=4) {
-            menu.end(`Thanks for your feedback. Cheers! `);
-            message = "Glad the lessons were well taught, thank you for the high rating. All the best in your exams!";
-
-            sendSMS(menu.args.phoneNumber, message)
-        }
-
-        if(lessons >=2 && lessons <4 ) {
-            menu.end(`Hope the rating gets better next semester :)\nAll the best in your exams!`);
-            message = "Sorry the lessons could have been better, feel free to share an email on what we can improve. All the best in your exams!";
-
-            sendSMS(menu.args.phoneNumber, message)
-        }   
-        
-        if( lessons<2){
-            menu.end(`Sorry that the lessons weren't well taught :(\nAll the best in your exams! `);
-            message = "Apologies that the lessons were not the best. Pass by the department to let us know how we can serve you better. All the best in your exams!";
-
-            sendSMS(menu.args.phoneNumber, message)
-
-        }
-
-    }
-})
-
-// Registering USSD handler with Express
- 
-app.post('/ussd', async function(req, res){
-    console.log(req.body);
-    await menu.run(req.body, ussdResult => {
-        res.send(ussdResult);
-    });
-});
-
+//Send SMS function
 const sendSMS = (to, msg) => {
     const sms = AfricasTalking.SMS;
 
     sms
     .send({ 
         to: to, // Number of recipient - can be multiple
-        message:msg, // Message to be sent 
+        message:"Based on your symptoms:\n" + msg+"\nPlease seek medical attention, and get well soon :)", // Message to be sent 
         enqueue: true })
     .then(response => {
         console.log(response);
@@ -133,4 +36,110 @@ const sendSMS = (to, msg) => {
     });
 }
 
+//GeminiPlugIn
+const Your_API_Key = process.env.GEMINI_API;
+const genAI = new GoogleGenerativeAI(Your_API_Key)
+
+async function runAI(patient_num, sympt) {
+    // For text-only input, use the gemini-pro model
+    const model = genAI.getGenerativeModel({ model: "gemini-pro"})
+  
+    const symptoms = sympt;
+    const prompt = "What disease am I likely to have with the following symptoms: "+symptoms+"? Please limit the response to a maximum of 90 characters.";
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const diseases = response.text();
+    sendSMS(patient_num, diseases);
+    console.log(diseases);
+  }
+
+// Require the AT package
+const AfricasTalking = require("africastalking")(credentials);
+
+// Define inputs
+let sympt = []
+let patient = []
+let response = []
+
+// Defining states
+menu.startState({
+    run: () => {
+        sympt = []
+        patient = []
+        response =[]
+
+        // Welcome Screen      
+        menu.con(' Welcome to our health check app! You\'ll get disease suggestions based on symptoms. Disclaimer: We\'re not doctors! :) '+
+                 '\n1. Start health analysis' 
+                //  '\n0. Exit'
+            );
+    },
+    // Next object links to next state based on user input
+    next: {
+        '*\\d+': 'response'
+        
+    }
+})
+
+menu.state('response', {
+    run: () => {
+        response.push(Number(menu.val))
+        //Check Response to proceed
+        // End session on exit
+        if(response < 1) {
+            menu.end(`Thanks for dialing. Cheers!`);
+        }
+    },
+    // Next object links to next state based on user input
+    next: {
+        '*\\d+': 'response'
+        
+    }
+})
+
+//Details collection
+menu.state('response', {
+    run: () => {
+        response.push(Number(menu.val))
+        menu.con('Please enter your name: ')
+    },
+    next: {
+        '*[a-zA-Z]+': 'patient'
+    }
+})
+
+menu.state('patient', {
+    run: () => {
+        patient.push(String(menu.val))
+        menu.con(`Welcome ${patient[0]}! Please list the symptoms you\'re experiencing below, seperated by commas:`)
+    },
+    next: {
+        
+        '*[a-zA-Z]+': 'sympt'
+    }
+})
+
+menu.state('sympt', {
+    run: () => {
+        runAI(menu.args.phoneNumber, sympt)
+        sympt.push(String(menu.val))
+        menu.end(`Very sorry that you\'re unwell! Please check your SMS app for a text on possible illnesses, as you go visit a medical professional too.
+        \nGet well soon!`);
+    }
+    // next: {
+        
+    //     '*\\d+': 'lessons'
+    // }
+})
+
+// Registering USSD handler with Express
+ 
+app.post('/ussd', async function(req, res){
+    console.log(req.body);
+    await menu.run(req.body, ussdResult => {
+        res.send(ussdResult);
+    });
+});
+
 app.listen(PORT, () => console.log(`Running on port: ${PORT}` ));
+// run();
